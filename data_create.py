09d44f6b4f5cad0c92b4bce93ebb83dfbba7d8c8
jaeseok4104 <fhraos9@gmail.com>
2020-11-12 02:18:41 +0900

    Test

---
+++ data_create.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 720)
+cap.set(4, 1080)
+num = 0
+pos = './images/positive'
+neg = './images/negetive'
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame', gray)
+    k = cv2.waitKey(1)
+    if k == ord('s'):
+        images_dir = pos + str(num)+'.bmp'
+        cv2.imwrite(images_dir, gray)
+        num+=1
+    if k == ord('d'):
+        images_dir = neg + str(num)+'.bmp'
+        cv2.imwrite(images_dir, gray)
+        num+=1
+    if k == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
